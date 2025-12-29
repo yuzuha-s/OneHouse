322ec5cd115d5172b5b8d8eec809945f7d2b55ff
yuzu <yuzuha.1415s@gmail.com>
2025-12-29 17:38:50 +0900

    スマホ幅のUIを変更

--- onehouse/resources/views/layout/index.blade.php
+++ onehouse/resources/views/layout/index.blade.php
@@ -15,7 +15,7 @@
         <div class="detail wrapper">
             <div class="appname wrapper">
                 <div class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="110px" viewBox="0 -960 960 960" width="110px"
+                    <svg xmlns="http://www.w3.org/2000/svg" height="90px" viewBox="0 -960 960 960" width="90px"
                         fill="#000">
                         <path
                             d="M230.77-190.77h161.54v-240h175.38v240h161.54v-373.85L480-753.46 230.77-564.87v374.1ZM200-160v-420l280-211.54L760-580v420H536.92v-240H423.08v240H200Zm280-312.23Z" />
--- onehouse/routes/api.php
+++ onehouse/routes/api.php
@@ -9,11 +9,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// チェックリストの登録・更新・削除
 
-
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::post('/checklist', [CheckListController::class, 'store']);
-//     Route::put('/checklist/{id}', [CheckListController::class, 'update']);
-//     Route::delete('/checklist/{id}', [CheckListController::class, 'destroy']);
-// });
