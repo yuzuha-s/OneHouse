7cf32c438aef75af2caa92bf90ab8c695be387e2
yuzu <yuzuha.1415s@gmail.com>
2026-01-05 11:27:01 +0900

    スマホではテーブルをカードに変更

--- onehouse/resources/views/checklistcategory.blade.php
+++ onehouse/resources/views/checklistcategory.blade.php
@@ -7,5 +7,42 @@
 @endsection
 
 @section('aside')
-<div>ただいま作成中です</div>
+    <p>ただいま作成中です</p>
+
+    <div class="land-card">
+        <h4>Step1 まずは準備と情報収集！</h4>
+        <nav>
+            <ul>
+                <li>家族希望条件を整理</li>
+                <li>大体の予算を知る</li>
+                <li>家探しの流れを教える</li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="land-card">
+        <h4>Step2 住宅メーカーへ相談に行こう！</h4>
+        <nav>
+            <ul>
+                <li>複数社のモデルハウスを見学</li>
+            </ul>
+        </nav>
+    </div>
+    <div class="land-card">
+        <h4>Step3 資金計画と住宅ローンについて考えよう！</h4>
+    </div>
+
+    <div class="land-card">
+        <h4>Step4 安心して家を建てるための土地選びと法的チェック！</h4>
+    </div>
+
+    <div class="land-card">
+        <h5>Step5 </h5>
+        <h5>理想の住まいを形にする最終工程！</h5>
+    </div>
+
+
+
+
+
 @endsection
